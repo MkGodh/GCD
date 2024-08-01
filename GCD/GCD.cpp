c003acb7fa8c987fb1eadcc582cc46389afbb15c
MkaGodh <fenryrvla@yandex.ru>
2024-08-01 14:45:37 +0300

    Добавьте файлы проекта.

---
+++ GCD/GCD.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+void swap(int& a, int& b)
+{
+    int t = a;
+    a = b;
+    b = t;
+}
+
+int gcd(int a, int b)
+{
+    if (b > a) swap(a, b);
+    if (a % b == 0) return b;
+    return gcd(b, a % b);
+}
+
+int main()
+{
+    std::cout << gcd(440, 160);
+
+    return 0;
+}
+
